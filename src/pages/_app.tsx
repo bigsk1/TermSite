bc03940af58bc5f77b33bb740a81081540e97fa9
TKS <32640296+bigsk1@users.noreply.github.com>
2023-04-25 00:14:20 -0700

    vercel stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
-const App = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   const onClickAnywhere = () => {
@@ -12,24 +14,26 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-  <meta
-    name="viewport"
-    content="initial-scale=1.0, width=device-width, viewport-fit=cover"
-    key="viewport"
-    maximum-scale="1"
-     />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+          key="viewport"
+          maximum-scale="1"
+        />
       </Head>
 
       <div
-     className="text-light-foreground dark:text-dark-foreground min-w-0 text-xs md:min-w-full md:text-base dark"
-     onClick={onClickAnywhere}
-        >
+        className="text-light-foreground dark:text-dark-foreground min-w-0 text-xs md:min-w-full md:text-base dark"
+        onClick={onClickAnywhere}
+      >
         <main className="bg-light-background dark:bg-dark-background w-full h-full p-2">
           <Component {...pageProps} inputRef={inputRef} />
         </main>
       </div>
+      <Analytics />
     </>
   );
 };
 
 export default App;
+
