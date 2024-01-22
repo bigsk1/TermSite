f70f7be9c4f6449bcf29b74b22c80866ead81441
bigsk1 <32640296+bigsk1@users.noreply.github.com>
2024-01-22 04:15:59 -0800

    Update sumfetch.ts
    update sumfetch

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -8,13 +8,13 @@ const sumfetch = async (args: string[]): Promise<string> => {
       @@                       @@            ABOUT
     @@                           @@          ${config.name}
   @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.homepage_url}" target="_blank">TKS Website</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+ @@                         @@@     @@       <u><a href="${config.homepage_url}" target="_blank">Website</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">This Repo</a></u>
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT 
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
   @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="${config.social.github_docs}" target="_blank">TKS Github Docs Site</a></u>
+   @@             @@@@@@          @@         <u><a href="${config.social.github_docs}" target="_blank">bigsk1 docs repo</a></u>
      @@@                        @@@          <u><a href="https://twitter.com/${config.social.twitter}" target="_blank">twitter.com/${config.social.twitter}</a></u>
         @@@                  @@@ @@          
          @|  @@@@@@@@@@@@@@@@   @@          
@@ -27,13 +27,13 @@ const sumfetch = async (args: string[]): Promise<string> => {
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.homepage_url}" target="_blank">TKS Website</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.homepage_url}" target="_blank">Website</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">This Repo</a></u>
 ▐▓                                 ▐▓       -----------
 ▐▓        >  B I G S K 1           ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="${config.social.github_docs}" target="_blank">TKS Github Docs Site</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="${config.social.github_docs}" target="_blank">bigsk1 docs repo</a></u>
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="https://twitter.com/${config.social.twitter}" target="_blank">twitter.com/${config.social.twitter}</a></u>
       ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             
         ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
